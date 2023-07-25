f7e0836c4f23cb161053846a0db0e53bc459b696
Markus Puffer <m.puffer@pm-webdesign.eu>
2023-07-25 13:23:22 +0200

    [BUGFIX] Non AD-User could login on menu now.

--- Classes/Controller/AjaxController.php
+++ Classes/Controller/AjaxController.php
@@ -1,28 +1,56 @@
 <?php
 
 /*
- * Copyright (c) 2021 Markus Blöchl <mbloechl@parat.eu>.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2021-2023 PARAT Beteiligungs GmbH. Typo3 Project - Pmbelayout
+ * Markus Puffer (dvpm) - mpuffer@parat.eu
  *
- * Contributors:
- *    Markus Blöchl <mbloechl@parat.eu> - initial API and implementation and/or initial documentation
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 namespace Pmwebdesign\Pmbelayouts\Controller;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException;
+
 /**
  * Description of AjaxController
  *
- * @author Markus Blöchl <mbloechl@parat.eu>
+ * @author Markus Puffer
  */
-class AjaxController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class AjaxController extends ActionController
 {
-    public function loadLoginAction()
+    /**
+     * Load the login template
+     *
+     * @return void
+     */
+    public function loadLoginAction(): void
     {
-        
+        $baseUrl = $GLOBALS['TSFE']->getSite()->getConfiguration()['base'];
+        $pid = $GLOBALS['TSFE']->tmpl->setup_constants['plugin.']['tx_staffm.']['persistence.']['storagePid'];
+        $this->view->assign('baseUrl', $baseUrl);
+        $this->view->assign('pid', $pid);
     }
-    
+
+    /**
+     * Load the logout template
+     *
+     * @throws NoSuchArgumentException
+     */
     public function loadLogoutAction(): void
     {
         if($this->request->hasArgument('dropdownAnimate')) {
